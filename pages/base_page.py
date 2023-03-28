eb6f0c66d60b835d83f5cedbeccc717355a120bc
shev7a <sheva7rus@gmail.com>
2023-03-28 22:07:35 +0300

    added timeout and new method for searching element on page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
 
-    __slots__ = 'browser', 'url'
+    __slots__ = 'browser', 'url', 'timeout'
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, selector):
+        try:
+            self.browser.find_element(by, selector)
+        except NoSuchElementException:
+            return False
+        return True
