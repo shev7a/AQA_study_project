2eda17a0aee20d4d2d20064a0c3945f75812fbbe
shev7a <sheva7rus@gmail.com>
2023-03-28 23:20:02 +0300

    added LoginPage check

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -6,7 +6,8 @@ url = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, url)
     page.open()
-    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
