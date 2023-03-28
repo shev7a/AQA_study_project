b376a87d703e35ab865bcf5b20a8ddd6d741c258
shev7a <sheva7rus@gmail.com>
2023-03-28 23:18:38 +0300

    added types

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage:
 
     __slots__ = 'browser', 'url', 'timeout'
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser: WebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
