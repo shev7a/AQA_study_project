e44d1c7d7fa540941c070235b1b1fb68a16d0533
shev7a <sheva7rus@gmail.com>
2023-04-02 21:28:12 +0300

    replaced login methods to base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import math
+from .locators import BasePageLocators
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,7 +7,6 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 
 
 class BasePage:
-
     __slots__ = 'browser', 'url', 'timeout'
 
     def __init__(self, browser: WebDriver, url: str, timeout: int = 10):
@@ -38,6 +38,13 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
